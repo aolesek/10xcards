9c2ff1fc87d52c4ddd2ff352252e3884eaa30f6b
Arkadiusz Olesek <arkadiusz.olesek@inspeerity.com>
2026-01-21 22:10:38 +0100

    added additional rule files from 10xdevs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,8 @@
-import { useState } from "react";
-import "./App.css";
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/routes/router";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Card>Test TestTestTest Test Test Test Test Test Test Test Test Test 
-        <Button variant="default">Click me</Button>
-
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        </Card>
-     
-      </div>
-    </>
-  );  
+  return <RouterProvider router={router} />;
 }
 
 export default App;
